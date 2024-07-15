e9dc17f0f7498d13990e871c0a12c8c6fd30bbbd
Pedro Câmara <ol.pedrocamara@gmail.com>
2024-07-15 17:40:23 -0300

    Commit Return

--- app.py
+++ app.py
@@ -1,19 +1,32 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
 api = Api(app)
+db = MongoEngine
+
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'users',
+    'host': 'mongodb',
+    'port': 27017,
+    'user': 'admin',
+    'password': 'admin'
+}
+
 
 class Users(Resource):
     def get(self):
         return {"massage": "user 1"}
 
+
 class User(Resource):
     def get(self):
         return {"massage": "teste"}
-    
+
     def get(self, cpf):
         return {"message": "CPF"}
+
 
 api.add_resource(Users, '/users')
 api.add_resource(User, '/user', '/user/<string:cpf>')
